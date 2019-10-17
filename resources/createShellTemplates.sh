74ac39ea0128ce1510459b20a45124fedb94556a
Strack <ck@softwareag.com>
2019-10-17 08:15:31 +0200

    changed threshold for container

---
+++ resources/createShellTemplates.sh
@@ -0,0 +1,37 @@
+TENANT='your-tenant'
+URL="https://$TENANT.cumulocity.com/inventory/managedObjects"
+USER='your-user:your-password'
+curl -X POST \
+  "$URL" \
+  -H 'Accept: application/json' \
+  --user "$USER" \
+  -H 'Content-Type: application/json' \
+  -d '{"type":"c8y_DeviceShellTemplate","c8y_Global":{},"deviceType":"c8y_XDKDevice","name":"toggle on/off yellow LED","command":"toggle","category":"operation"}'
+  
+curl -X POST \
+  "$URL" \
+  -H 'Accept: application/json' \
+  --user "$USER" \
+  -H 'Content-Type: application/json' \
+  -d '{"type":"c8y_DeviceShellTemplate","c8y_Global":{},"deviceType":"c8y_XDKDevice","name":"enable/disable sensor ... TRUE/FALSE","command":"sensor NOISEENABLED TRUE","category":"sensor"}'
+  
+curl -X POST \
+  "$URL" \
+  -H 'Accept: application/json' \
+  --user "$USER" \
+  -H 'Content-Type: application/json' \
+  -d '{"type":"c8y_DeviceShellTemplate","c8y_Global":{},"deviceType":"c8y_XDKDevice","name":"change speed","command":"speed 2000","category":"operation"}'
+  
+curl -X POST \
+  "$URL" \
+  -H 'Accept: application/json' \
+  --user "$USER" \
+  -H 'Content-Type: application/json' \
+  -d '{"type":"c8y_DeviceShellTemplate","c8y_Global":{},"deviceType":"c8y_XDKDevice","name":"stop publishing","command":"stop","category":"operation"}'
+  
+curl -X POST \
+  "$URL" \
+  -H 'Accept: application/json' \
+  --user "$USER" \
+  -H 'Content-Type: application/json' \
+  -d '{"type":"c8y_DeviceShellTemplate","c8y_Global":{},"deviceType":"c8y_XDKDevice","name":"start publishing","command":"start","category":"operation"}'
